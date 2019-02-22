2f2a4944b03ad6b3d33e785e085e0d71eb499663
David Bruno <davidbruno2000@gmail.com>
2019-02-21 19:40:54 -0500

    segment file and variables created

---
+++ set1993/segment.c
@@ -0,0 +1,12 @@
+/***************************************************************************/                                                                    */
+/* Problem Name: Segment - 1993
+/* Date: 2/21/19
+/* Problem by: David Bruno
+/***************************************************************************/
+
+#include<stdio.h>
+
+int main(void){
+    double x, double y, and double r;
+    return 0;
+}
